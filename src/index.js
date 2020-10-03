91179e2419083cf4b6ec83c5ebda6695ebdcfd48
Andrii Abramov <abramov.nd@gmail.com>
2020-10-03 23:13:32 +0300

    feat: add code to index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length) {
+        let res = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if(res > array[i]) {
+                res = array[i];
+            }
+        }
+        return res;
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length) {
+        let res = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if(res < array[i]) {
+                res = array[i];
+            }
+        }
+        return res;
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length) {
+        let res = 0;
+        for (let i = 0; i < array.length; i++) {
+            res = res + array[i];
+        }
+        return res / array.length;
+    } else {
+        return 0;
+    }
 }
